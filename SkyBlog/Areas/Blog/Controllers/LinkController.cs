def0f86a1ec2cb0b809345cd6c22e4b84bda1a1e
351500051@qq.com <31500051@qq.com>
2018-04-27 11:23:58 +0800

    提交页面测试
    提交页面测试

--- SkyBlog/Areas/Blog/Controllers/LinkController.cs
+++ SkyBlog/Areas/Blog/Controllers/LinkController.cs
@@ -5,13 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SkyBlog.Areas.Blog.Controllers
-{    
-    public class HomeController : BaseController
+{
+    public class LinkController : BaseController
     {
         public IActionResult Index()
-        {           
+        {
             return View();
         }
-    
     }
 }
---
+++ SkyBlog/Areas/Blog/Controllers/ViewComponents/BlogRightViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkyBlog.Areas.Blog.Controllers.ViewComponents
+{
+    public class BlogRightViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.Run(() =>
+            {
+                return View();
+            }); 
+        }
+    }
+}

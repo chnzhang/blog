6048b8024e84c877bf50a7de6908b6e28240715d
351500051@qq.com <31500051@qq.com>
2018-04-28 14:01:11 +0800

    测试
    测试

---
+++ SkyBlog/Common/MyDateTime.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkyBlog.Common
+{
+    public static class MyDateTime
+    {
+        /// <summary>
+        /// 得到年月日 时分秒 格式化日期 24小时制
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static string GetYMDHMS(this DateTime dt)
+        {
+            return dt.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+     
+        /// <summary>
+        /// 得到年月日 时分秒 格式化日期 24小时制 上下午
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static string GetYMDHMSTT(this DateTime dt)
+        {
+            return dt.ToString("yyyy-MM-dd HH:mm:ss tt");
+        }
+
+
+        /// <summary>
+        /// 得到年月日 时分秒 格式化日期 12小时制止
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static string GetYMDhms(this DateTime dt)
+        {
+            return dt.ToString("yyyy-MM-dd hh:mm:ss");
+        }
+
+        /// <summary>
+        /// 得到年月日 时分秒 格式化日期 12小时制 上下午
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static string GetYMDhmsTT(this DateTime dt)
+        {
+            return dt.ToString("yyyy-MM-dd hh:mm:ss tt");
+        }
+
+   
+   
+    }
+}
